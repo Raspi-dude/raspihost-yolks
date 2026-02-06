c61d826975db3a9a52939a46b53c8b214cd5069d
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-05 22:24:59 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -3,17 +3,8 @@ cd /home/container
 
 echo "testing wsg guys"
 
-# Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
+
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-eval ${MODIFIED_STARTUP} &
-SERVER_PID=$!
-
-# Wait for the server process to finish
-wait $SERVER_PID
-EXIT_CODE=$?
-
-echo "Server exited with code ${EXIT_CODE}"
-exit ${EXIT_CODE}
+eval ${MODIFIED_STARTUP}
